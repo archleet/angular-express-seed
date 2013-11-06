318a3b9afc0102735000745c15657789535af9d7
archleet <archleet3000@gmail.com>
2013-11-05 19:49:44 -0800

    modified controllers.js

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,7 +2,66 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+function IndexCtrl($scope, $http) {
+  $http.get('/api/posts').
+    success(function(data, status, headers, config){
+      $scope.posts = data.posts;
+    });
+}
+
+function AddPostCtrl($scope, $http, $location) {
+  $scope.form = {};
+  $scope.submitPost = function() {
+    $http.post('/api/post', $scope.form).
+      success(functio(data){
+        $location.path('/');
+      });
+  };
+}
+
+function ReadPostCtrl($scope, $http, $routeParams) {
+  $http.get('api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+}
+
+function EditPostCtrl($scope, $http, $location, $routeParams) {
+  $scope.form = {};
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.form = data.post;
+    });
+
+    // Add EditPost function to $scope
+  $scope.editPost = function () {
+    $http.put('/api/post/' + $routeParams.id, $scope.form).
+      success(function(data) {
+        $location.url('/readPost/' + $routeParams.id);
+      });
+  };
+}
+
+function DeletePostCtrl($scope, $http, $location, $routeParams) {
+  $http.get('/api/post/' + $routeParams.id).
+    success(function(data) {
+      $scope.post = data.post;
+    });
+
+  $scope.deletePost = function () {
+    $http.delete('/api/post/' + $routeParams.id).
+      success(function(data) {
+        $location.url('/');
+      });
+  };
+
+  $scope.home = function () {
+    $location.url('/');
+  };
+}
+
+
+/*angular.module('myApp.controllers', []).
   controller('AppCtrl', function ($scope, $http) {
 
     $http({
@@ -24,4 +83,6 @@ angular.module('myApp.controllers', []).
   controller('MyCtrl2', function ($scope) {
     // write Ctrl here
 
-  });
+  });*/
+
+
